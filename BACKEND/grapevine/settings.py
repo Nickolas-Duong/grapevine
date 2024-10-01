fecb7b9f26b12dbf5b6deaef29f6c7dd0d9ab1c1
Nickolas-Duong <nduong@cpp.edu>
2024-10-01 11:13:17 -0700

    Update settings.py

--- BACKEND/grapevine/settings.py
+++ BACKEND/grapevine/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'grapevine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['grapevine/templates'],
+        'DIRS': ['grapevine/static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
